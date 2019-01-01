311df04cb3c804a796e6be64af3bd52c12b83fe3
Eddie Gahan <gahan@users.noreply.github.com>
2019-01-01 14:37:34 +0000

    Updated methods and example console application.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ConsoleColours; 
 
 namespace CrossRefCollection
 {
@@ -7,9 +8,12 @@ namespace CrossRefCollection
     {
         static void Main(string[] args)
         {
+            ANSIConsole.ActivateConsole();
+
             // Example 1.  Initialising a blank object and then passing a complete object[,] to the Table parameter to 
-            // fill in row and column keys and all data values.  In this example the table data represents the available bit rates
-            // 
+            // fill in row and column keys and all data values.  In this example the table data represents the bit rate
+            // of an MP3 based on values taken from the header of the MP3 file.  The column headers represent the MPEG version 
+            // and the 'Layer' and the rows represent the 4 bits taken from the file that denote the bitrate of the audio.
 
             CrossReference<int, int, int> _TestTable = new CrossReference<int, int, int>();
 
@@ -34,7 +38,11 @@ namespace CrossRefCollection
                 { 1111,   -1,   -1,   -1,   -1,   -1,   -1 }
             };
 
-            PrintOutTable(_TestTable);
+            PrintOutTable("TestTable", _TestTable);
+
+            // Example 2.  This is a simple 3x3 grid with a string as the row key, an int as the column key and booleans as the data type.  After the grid
+            // has been initialised and 3 of the falues changed to true (the default value of the data will be false) new columns keys are set which has
+            // the effect of resetting all of the data.
 
             CrossReference<string, int, bool> _TestTableB = new CrossReference<string, int, bool>(new string[] { "A", "B", "C" }, new int[] { 1, 2, 3 });
 
@@ -42,10 +50,14 @@ namespace CrossRefCollection
             _TestTableB["B", 2] = true;
             _TestTableB["C", 3] = true;
 
-
+            PrintOutTable("TestTableB", _TestTableB);
 
             _TestTableB.Columns = new List<int>() { 9, 8, 7 };
 
+            PrintOutTable("TestTableB (with new column headers)", _TestTableB);
+
+            // Example 3.  This is a similiar example to the previous one but it demonstrates a different method for setting and resetting the column keys
+            // Row keys can be reset in the exact same way.
 
             CrossReference<string, string, double> _TestTableC = new CrossReference<string, string, double>();
 
@@ -56,25 +68,52 @@ namespace CrossRefCollection
             _TestTableC["Y", "Y"] = 1.5;
             _TestTableC["Z", "X"] = 1.5;
 
+            PrintOutTable("TestTableC", _TestTableC);
+
             _TestTableC.AddColumns("A", "B", "C");
 
             _TestTableC["X", "A"] = 2.1;
             _TestTableC["Y", "B"] = 2.1;
             _TestTableC["Z", "C"] = 2.1;
 
+            PrintOutTable("TestTableC (with new column headers and values)", _TestTableC);
+
+            Console.ReadLine();
         }
 
-        static void PrintOutTable<TRows, TColumns, TData>(CrossReference<TRows, TColumns, TData> TargetTable)
+        static void PrintOutTable<TRows, TColumns, TData>(string Label, CrossReference<TRows, TColumns, TData> TargetTable)
         {
-            foreach(TRows _Row in TargetTable.Rows)
+            Console.Write(ConsoleEffects.Bright_Foreground_White + ConsoleEffects.Bold + ConsoleEffects.Underline);
+            Console.Write(Label);
+            Console.Write(ConsoleEffects.Default);
+            Console.Write("\r\n");
+            Console.Write("\r\n");
+
+            foreach (TColumns _Column in TargetTable.Columns)
             {
-                foreach(TColumns _Column in TargetTable.Columns)
-                {
-                    Console.WriteLine(TargetTable[_Row, _Column]);
-                }
+                Console.Write(ConsoleEffects.Bright_Foreground_White);
+                Console.Write("\t");
+                Console.Write(_Column);
+                Console.Write(ConsoleEffects.Default);
             }
 
+            Console.Write("\r\n");
 
+            foreach (TRows _Row in TargetTable.Rows)
+            {
+                Console.Write(ConsoleEffects.Bright_Foreground_White);
+                Console.Write(_Row);
+                Console.Write(ConsoleEffects.Default);
+
+                foreach (TColumns _Column in TargetTable.Columns)
+                {
+                    Console.Write("\t");
+                    Console.Write(TargetTable[_Row, _Column]);
+                }
+                Console.Write("\r\n");
+            }
+
+            Console.Write("\r\n");
         }
     }
 }
